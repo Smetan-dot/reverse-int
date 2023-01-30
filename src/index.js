8b4644cd328974c513c8c527f61dfbabe9a3980b
Smetan-dot <121453049+Smetan-dot@users.noreply.github.com>
2023-01-30 11:41:41 +0300

    sol: solution for reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const strN = String(Math.abs(n));
+  let result = [];
+  for (let i = 0; i <= strN.length; i++){
+    result.push(strN[strN.length - i]);
+  }
+  return Number(result.join(''));
 }
